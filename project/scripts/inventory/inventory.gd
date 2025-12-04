20eedb69292d4a8b86bb4bfc579c596237910e7c
MagnaMonstrum <moboy98@gmail.com>
2025-12-04 17:04:51 +0100

    fixed where loot_nums are placed in inventory

--- project/scripts/inventory/inventory.gd
+++ project/scripts/inventory/inventory.gd
@@ -8,7 +8,7 @@ signal update_signal
 @export var slots: Array[InvSlot]
 
 func insert(loot_num: LootNumResource) -> void:
-	slots[loot_num.value - 1].loot_num = loot_num
-	slots[loot_num.value - 1].amount += 1
+	slots[loot_num.value].loot_num = loot_num
+	slots[loot_num.value].amount += 1
 	
 	emit_signal("update_signal", slots)
