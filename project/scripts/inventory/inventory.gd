89dd58121912e747ed0df7aed3eefae32be113b0
Pafias <29665404+Pafias@users.noreply.github.com>
2025-12-11 17:33:26 +0100

    oopsie

--- project/scripts/inventory/inventory.gd
+++ project/scripts/inventory/inventory.gd
@@ -7,10 +7,10 @@ signal update_signal
 @export var slots: Array[InvSlot]
 
 func insert(value: int) -> bool:
-	if not slots.any(func(slot): return slot.value == -1):
+	if not slots.any(func(slot): return slot.value == -1 or slot.value == value):
 		return false
 
-	for i in range(9):
+	for i in range(slots.size()):
 		if slots[i].value == -1:
 			slots[i].value = value
 			slots[i].amount += 1
