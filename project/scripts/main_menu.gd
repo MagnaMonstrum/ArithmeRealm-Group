730212572a8833b052135d2a9fe497b002ab91e9
danin342 <dnassor342@gmail.com>
2026-06-21 20:50:09 +0200

    added practice mode

--- project/scripts/main_menu.gd
+++ project/scripts/main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready() -> void:
 	%Play.pressed.connect(play)
+	%Practice.pressed.connect(practice)
 
 func play() -> void:
 	get_tree().change_scene_to_file("res://project/scenes/menus/exposition_screen.tscn")
+
+func practice() -> void:
+	get_tree().change_scene_to_file("res://project/scenes/levels/practice.tscn")
